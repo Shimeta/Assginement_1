35708f23cc9cb4895ec69e6ffe2ae0c2b2dbc18c
Shimeta <kahilrazor@gmail.com>
2018-04-21 04:09:24 +0200

    Update Vogelgehege.java

--- Zoo/src/gehegepackage/Vogelgehege.java
+++ Zoo/src/gehegepackage/Vogelgehege.java
@@ -1,5 +1,16 @@
 package gehegepackage;
 
-public class Vogelgehege {
+import tierpackage.Tier;
+import Zoo;
 
+public class Vogelgehege extends Gehege {
+	
+	
+
+	public Vogelgehege(String name, Tier[] tier, Zoo owner) {
+		super(name, tier, owner);
+		
+	}
+	
+	
 }
