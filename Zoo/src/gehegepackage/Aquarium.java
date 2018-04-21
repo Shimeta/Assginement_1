8e03e324eb28c92f95f6f1b1f77186309be597f9
Shimeta <kahilrazor@gmail.com>
2018-04-21 04:05:23 +0200

    Update Aquarium.java

--- Zoo/src/gehegepackage/Aquarium.java
+++ Zoo/src/gehegepackage/Aquarium.java
@@ -1,5 +1,12 @@
 package gehegepackage;
+import tierpackage.Tier;
+import Zoo;
 
-public class Aquarium {
+public class Aquarium extends Gehege{
 
+	public Aquarium(String name, Tier[] tier, Zoo owner) {
+		super(name, tier, owner);
+
+	}
+  
 }
