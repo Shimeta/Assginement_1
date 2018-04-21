917fb5a73ac37a48b44e85a2b4386581e7a09b71
Shimeta <kahilrazor@gmail.com>
2018-04-21 03:56:12 +0200

    Update Gehege.java

--- Zoo/src/gehegepackage/Gehege.java
+++ Zoo/src/gehegepackage/Gehege.java
@@ -1,5 +1,18 @@
 package gehegepackage;
+import tierpackage.Tier;
 
 public abstract class Gehege {
-
-}
+	
+	private String name;
+	private Tier[] tier;	
+	private Zoo owner;
+	
+	
+	public Gehege(String name,Tier[] tier, Zoo owner ) {
+		
+		this.name= name;
+		this.tier = tier;
+		this.owner = owner;
+		
+	}
+	}
