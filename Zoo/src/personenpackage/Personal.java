a338b3d6eafcb6666be088d9a8506c03c01ee714
Shimeta <kahilrazor@gmail.com>
2018-04-21 04:16:48 +0200

    Update Personal.java

--- Zoo/src/personenpackage/Personal.java
+++ Zoo/src/personenpackage/Personal.java
@@ -1,5 +1,28 @@
 package personenpackage;
 
 public abstract class Personal {
+  
+  public class Personal extends Personen {
+	
+	public int lohn;
 
+	public Personal(String name, int alter, Gehege gehege, int lohn) {
+		
+		super(name, alter, gehege);
+    
+		this.lohn= lohn;
+			
+	}
+
+	@Override
+	public void laufen(Personen person, Gehege gehege) {
+		
+		System.out.println(person + "geht zum " + gehege);
+		
+		
+	}
+	
+
+  }
+  
 }
